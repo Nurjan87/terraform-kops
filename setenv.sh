cb26fef3e7b38cf31c0eb9463d579fce71210aab
Dastan Raimzhan <dastancpa@gmail.com>
2021-01-19 09:54:35 -0600

    added

--- setenv.sh
+++ setenv.sh
@@ -76,9 +76,9 @@ backend "s3" {
 bucket = "${S3BUCKET}"
 key = "${S3BUCKETPROJ}/${S3BUCKETREGION}/${S3BUCKETTYPE}/${ENVIRONMENT}/${S3TFSTATEFILE}"
 region = "${S3BUCKETREGION}"
-} 
+  } 
 }
 EOF
 cat backend.tf
 rm -rf .terraform/terraform.tfstate
-terraform_0.11.14 init
+terraform init
